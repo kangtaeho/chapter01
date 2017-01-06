b9ea4c7fa3e434d512764c996a608f78e849268e
user <user@admin>
2017-01-06 23:20:05 +0900

    2016-01-06 수정

--- src/chapter01/SumToInput2.java
+++ src/chapter01/SumToInput2.java
@@ -20,10 +20,11 @@ public class SumToInput2 {
 
 		int count = 0;		
 		int sum = 0;
-		for (int i = 0; i <= count; i++) {
+		for (int i = 0; i <= number; i++) {
 
 			sum = sum + i;
 		}
+		
 		System.out.println(sum);
 
 	}
